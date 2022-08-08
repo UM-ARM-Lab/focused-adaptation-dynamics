d91877690b7658209668bbaec8f99e96368f3a07
Peter <peter@armnova>
2022-08-08 10:31:55 -0400

    save figs and comment out filtering in analysis

--- analysis/scripts/analyze_online_results.py
+++ analysis/scripts/analyze_online_results.py
@@ -19,9 +19,9 @@ def metrics_main(args):
     outdir, df, table_specs = planning_results(args.results_dirs, args.regenerate)
 
     # Filter the rows to keep only the trails with the same planning conditions
-    df = df_where(df, 'max_extensions_param', 5_000)
-    df = df_where(df, 'max_attempts', 3)
-    df = df_where(df, 'max_planning_attempts', 3)
+    # df = df_where(df, 'max_extensions_param', 5_000)
+    # df = df_where(df, 'max_attempts', 3)
+    # df = df_where(df, 'max_planning_attempts', 3)
 
     # if the results_folder_name contains the key, the set method_name to be the value
     method_name_map = {
@@ -40,7 +40,9 @@ def metrics_main(args):
     df['method_name'] = method_names
 
     fig, ax = lineplot(df, 'online_iter', 'success', 'Success', hue='method_name', ci=None, scatt=True)
+    plt.savefig(outdir / "success.png")
     fig, ax = lineplot(df, 'online_iter', 'normalized_model_error', 'Model Error', hue='method_name', scatt=True)
+    plt.savefig(outdir / "normalzed_model_error.png")
     plt.show()
 
 
