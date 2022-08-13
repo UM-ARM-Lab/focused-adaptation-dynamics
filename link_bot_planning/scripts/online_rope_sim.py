ee59a975f8d066f86265fef6ddd358b454111650
Peter <peter@armnova>
2022-08-13 17:35:16 -0400

    increase n_parallel again

--- link_bot_planning/scripts/online_rope_sim.py
+++ link_bot_planning/scripts/online_rope_sim.py
@@ -138,7 +138,7 @@ def main():
                 dynamics_dataset_name = dynamics_dataset_dir_i.name
                 sub_chunker_i.store_result('dynamics_dataset_name', dynamics_dataset_name)
             else:
-                n_parallel = int(multiprocessing.cpu_count() / 8)
+                n_parallel = int(multiprocessing.cpu_count() / 6)
                 print(f"{n_parallel=}")
                 online_parallel_planning(planner_params=planner_params_filename,
                                          method_name=method_name,
