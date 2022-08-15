293ad57717ea62d245744e8ac9f012e2348f792e
Ale x LaGrassa <lagrassa@cmu.edu>
2022-08-15 13:26:32 -0400

    voxel grid for water env

--- dm_envs/src/dm_envs/softgym_services.py
+++ dm_envs/src/dm_envs/softgym_services.py
@@ -1,13 +1,86 @@
 from dm_control import composer
+import numpy as np
 
 from link_bot_pycommon.base_services import BaseServices
+from arm_gazebo_msgs.srv import ComputeOccupancyResponse
 
-
-class SoftGymServices(BaseServices):
+class SoftGymServices():
 
     def __init__(self):
         super().__init__()
+        self._scene = None
 
     def setup_env(self, *args, **kwargs):
         pass
+
+    def set_scene(self, scene):
+        self._scene = scene
+
+    def __call__(self, args, **kwargs):
+        self.compute_occupancy(*args, **kwargs)
+
+    def is_occupied(self, x, y, z, env_indices):
+        coords = env_indices[x,y,z]
+        #only checks if occupied by the box!!
+        fixed_params = self._scene._wrapped_env.glass_params
+        poured_height = fixed_params["poured_height"]
+        poured_x = fixed_params["poured_glass_dis_x"]
+        poured_y = fixed_params["poured_height"]
+        poured_z = fixed_params["poured_glass_dis_z"]
+        poured_x_dims = [fixed_params["poured_glass_x_center"]-poured_x/2, fixed_params["poured_glass_x_center"]+poured_x/2] 
+        poured_z_dims = [0, poured_z/2] 
+        poured_y_dims = [-poured_y/2, poured_y/2] 
+        dim_ranges = [poured_x_dims, poured_y_dims, poured_z_dims]
+        in_ranges = np.array([False, False, False])
+        for dim_idx, dim_range in enumerate(dim_ranges):
+            if coords[dim_idx] >= dim_range[0] and coords[dim_idx] <= dim_range[1]:
+                in_ranges[dim_idx] = True
+        if np.all(in_ranges):
+            return True
+
+        return False
+
+    def pause(self):
+        pass
+
+    def play(self):
+        pass
+
+    def create_env_coords(self, x_dims, y_dims, z_dims, center, res):
+        #assuming cube for now
+        x_extent = x_dims*res
+        y_extent = y_dims*res
+        z_extent = z_dims*res
+        env_coords = np.zeros((x_dims, y_dims, z_dims, 3))
+        xs = np.linspace(center[0]-x_extent/2, center[0] + x_extent/2, x_dims)
+        ys = np.linspace(center[1]-y_extent/2, center[1] + y_extent/2, y_dims)
+        zs = np.linspace(center[2]-z_extent/2, center[2] + z_extent/2, z_dims)
+        for x in range(x_dims):
+            for y in range(y_dims):
+                for z in range(z_dims):
+                    env_coords[x,y,z, :] = np.array([xs[x], ys[y], zs[z]])
+        #idx_to_point_3d_in_env or idx_to_point_3d
+        return env_coords
+
+
+    def compute_occupancy(self, req):
+        assert self._scene is not None
+        center = np.array([req.center.x, req.center.y, req.center.z])
+        res = req.resolution
+        #make grid 
+        x_dims = req.w_cols
+        y_dims = req.h_rows
+        z_dims = req.c_channels
+        grid = np.zeros((x_dims, y_dims, z_dims))
+        env_coords = self.create_env_coords(x_dims, y_dims, z_dims, center, res)
+        response = ComputeOccupancyResponse()
+        for x in range(x_dims):
+            for y in range(y_dims):
+                for z in range(z_dims):
+                    if self.is_occupied(x,y,z, env_coords):
+                        grid[x,y,z] = 1
+        response.grid = grid
+        return response
+
+        
 
--- link_bot_pycommon/src/link_bot_pycommon/water_scenario.py
+++ link_bot_pycommon/src/link_bot_pycommon/water_scenario.py
@@ -16,6 +16,7 @@ from augmentation.aug_opt_utils import get_local_frame
 from geometry_msgs.msg import Point, Vector3
 from jsk_recognition_msgs.msg import BoundingBox
 from learn_invariance.transform_link_states import transform_link_states
+from dm_envs.softgym_services import SoftGymServices
 from link_bot_data.base_collect_dynamics_data import collect_trajectory
 from link_bot_data.coerce_types import coerce_types
 from link_bot_data.dataset_utils import get_maybe_predicted, in_maybe_predicted, add_predicted, add_predicted_cond
@@ -28,6 +29,7 @@ from link_bot_pycommon.bbox_visualization import viz_action_sample_bbox
 from link_bot_pycommon.debugging_utils import debug_viz_batch_indices
 from link_bot_pycommon.experiment_scenario import get_action_sample_extent, is_out_of_bounds, sample_delta_position
 from link_bot_pycommon.get_link_states import GetLinkStates
+from link_bot_pycommon.get_occupancy import get_environment_for_extents_3d
 from link_bot_pycommon.grid_utils_np import extent_to_env_shape, extent_res_to_origin_point
 from link_bot_pycommon.lazy import Lazy
 from link_bot_pycommon.make_rope_markers import make_gripper_marker, make_rope_marker
@@ -77,7 +79,11 @@ class WaterSimScenario(ScenarioWithVisualization):
         self._params = params
         self.max_action_attempts = 100
         self.robot_reset_rng = np.random.RandomState(0)
+        self.control_volumes = []
+        self.target_volumes = []
         self._make_softgym_env()
+        self.service_provider = SoftGymServices()
+        self.service_provider.set_scene(self._scene)
 
     def _make_softgym_env(self):
         softgym_env_name = "PourWater"
@@ -89,7 +95,7 @@ class WaterSimScenario(ScenarioWithVisualization):
         env_kwargs['num_variations'] = 1
         env_kwargs['render'] = 1  # True
         env_kwargs["action_repeat"] = 2
-        env_kwargs['headless'] = not self._params['gui']
+        env_kwargs['headless'] = 1 #not self._params['gui']
         self._save_cfg = self._params["save_cfg"]
 
         if not env_kwargs['use_cached_states']:
@@ -100,13 +106,24 @@ class WaterSimScenario(ScenarioWithVisualization):
             self.frames = [self._scene.get_image(self._save_cfg["img_size"], self._save_cfg["img_size"])]
 
     def needs_reset(self, state: Dict, params: Dict):
-        total_water_in_containers = state["control_volume"] + state["target_volume"]
+        if state["control_volume"].item() < 0.5:
+            return True
+        total_water_in_containers = state["control_volume"].item() + state["target_volume"].item()
         if total_water_in_containers < 0.5:
             return True
         return False
 
     def get_environment(self, params: Dict, **kwargs):
-        env = {"env": np.zeros((40, 40, 40))}
+        res = params["res"]
+
+        voxel_grid_env = get_environment_for_extents_3d(extent=params['extent'],
+                                                        res=res,
+                                                        frame='map',
+                                                        service_provider=self.service_provider,
+                                                        excluded_models=[])
+
+        env = {}
+        env.update({k: np.array(v).astype(np.float32) for k, v in voxel_grid_env.items()})
         return env
 
     def hard_reset(self):
@@ -119,30 +136,41 @@ class WaterSimScenario(ScenarioWithVisualization):
         pass
 
     def on_before_data_collection(self, params: Dict):
-        null_action = np.zeros(3, )
-        self._saved_data = self._scene.step(null_action, record_continuous_video=self._save_frames,
-                                            img_size=self._save_cfg["img_size"])
+        self.reset()
+
+    def on_after_data_collection(self, params: Dict):
+        if self._save_frames:
+            save_name = "test.gif"
+            save_numpy_as_gif(np.array(self.frames), save_name)
+            print("Saved to", save_name)
 
     def execute_action(self, environment, state, action: Dict, **kwargs):
-        target_pos = action["controlled_container_target_pos"].flatten()
-        target_angle = action["controlled_container_target_angle"].flatten()
+        goal_pos = action["controlled_container_target_pos"].flatten()
+        goal_angle = action["controlled_container_target_angle"].flatten()
         curr_state = state
+        curr_pos = curr_state["controlled_container_pos"].flatten()
+        curr_angle = curr_state["controlled_container_angle"].flatten()
+        angle_traj = np.linspace(curr_angle, goal_angle, int(self._params["controller_max_horizon"]*0.5))
+        pos_traj = np.vstack([np.linspace(curr_pos[dim], goal_pos[dim], int(self._params["controller_max_horizon"]*0.5)) for dim in range(curr_pos.shape[-1])]).T
         for i in range(self._params["controller_max_horizon"]):
             curr_pos = curr_state["controlled_container_pos"].flatten()
             curr_angle = curr_state["controlled_container_angle"].flatten()
+            traj_idx = min(i, len(angle_traj)-1)
+            target_pos = pos_traj[traj_idx]
+            target_angle = angle_traj[traj_idx]
             pos_error = target_pos - curr_pos
             pos_control = self._params["k_pos"] * (pos_error)
             angle_error = target_angle - curr_angle
             angle_control = self._params["k_angle"] * (angle_error)
 
             vector_action = np.hstack([pos_control, angle_control])
-
             self._saved_data = self._scene.step(vector_action, record_continuous_video=self._save_frames,
                                                 img_size=self._save_cfg["img_size"])
             _, _, _, info = self._saved_data
             curr_state = self.get_state()
             if self._save_frames:
                 self.frames.extend(info['flex_env_recorded_frames'])
+        return False 
 
     def dynamics_dataset_metadata(self):
         metadata = ScenarioWithVisualization.dynamics_dataset_metadata(self)
@@ -170,9 +198,23 @@ class WaterSimScenario(ScenarioWithVisualization):
                                               high=self._params["action_range"]["z"][1])
                 action = {"controlled_container_target_pos":   np.array([random_x, random_z], dtype=np.float32),
                           "controlled_container_target_angle": np.array(0, dtype=np.float32).reshape(-1,1)}
+            if self.is_action_valid(environment, state, action, action_params):
+                return action, False
+        if self.is_action_valid(environment, state, action, action_params):
             return action, False
+        return None, True
 
     def is_action_valid(self, environment: Dict, state: Dict, action: Dict, action_params: Dict):
+        curr_pos = state["controlled_container_pos"]
+        target_pos = action["controlled_container_target_pos"]
+        curr_height = curr_pos[1]
+        max_dist = 0.3
+        if np.linalg.norm(curr_pos-target_pos) > max_dist:
+            return False
+        min_height_for_pour = 0.25
+        is_pour = action["controlled_container_target_angle"] >= 0.001
+        if is_pour and curr_height < min_height_for_pour:
+            return False
         return True
 
     def actions_cost(self, states: List[Dict], actions: List[Dict], action_params: Dict):
@@ -187,9 +229,12 @@ class WaterSimScenario(ScenarioWithVisualization):
 
     def reset(self):
         self._scene.reset()
+        null_action = np.zeros(3,)
+        self._saved_data = self._scene.step(null_action, record_continuous_video=self._save_frames,
+                                            img_size=self._save_cfg["img_size"])
 
     def randomize_environment(self, env_rng, params: Dict):
-        self._scene.reset()
+        self.reset()
 
     @staticmethod
     def put_state_local_frame(state: Dict):
@@ -206,10 +251,10 @@ class WaterSimScenario(ScenarioWithVisualization):
         current_pos = state["controlled_container_pos"]
         current_angle = state["controlled_container_angle"]
         delta_pos = target_pos - current_pos
-        delta_angle = target_angle.flatten() - current_angle.flatten()
+        delta_angle = target_angle - current_angle
         return {
             'delta_pos':   delta_pos,
-            'delta_angle': delta_angle.reshape(-1, 1)
+            'delta_angle': delta_angle
         }
 
     @staticmethod
@@ -231,10 +276,10 @@ class WaterSimScenario(ScenarioWithVisualization):
 
         local_dict = {
             'controlled_container_pos_local':   delta_pos,
-            'controlled_container_angle_local': current_angle.reshape(-1, 1),
+            'controlled_container_angle_local': current_angle,
         }
-        local_dict["target_volume"] = state["target_volume"].reshape(-1, 1)
-        local_dict["control_volume"] = state["control_volume"].reshape(-1, 1)
+        local_dict["target_volume"] = state["target_volume"]
+        local_dict["control_volume"] = state["control_volume"]
         local_dict["target_container_pos"] = 0 * state["target_container_pos"]
         return local_dict
 
@@ -258,6 +303,8 @@ class WaterSimScenario(ScenarioWithVisualization):
                  "target_container_pos":       np.array([state_vector[6], 0]),  # need to check this one
                  "control_volume":             state_vector[-1].reshape(-1,1),
                  "target_volume":              state_vector[-2].reshape(-1,1)}
+        #self.control_volumes.append(state_vector[-1])
+        #self.target_volumes.append(state_vector[-2])
         return state
 
     def distance_to_goal(self, state: Dict, goal: Dict, use_torch=False):
@@ -305,5 +352,3 @@ class WaterSimScenario(ScenarioWithVisualization):
     def __repr__(self):
         return self.simple_name()
 
-    def is_action_valid(self, environment: Dict, state: Dict, action: Dict, action_params: Dict):
-        return True
